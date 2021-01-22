b3d9983b56b5875a5b783e386f345feb03c7afbb
jon-wehner <jonjwehner@gmail.com>
2021-01-22 06:15:20 -0800

    error handling setup

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const routes = require('./routes')
+const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const environment = require('./config');
 const isProduction = environment === 'production';
@@ -33,6 +34,34 @@ app.use(
     }
   })
   );
-app.use(routes)
+app.use(routes);
+
+app.use((req, res, next) => {
+  const err = new Error('The requested resource could not be found.');
+  err.title = 'Resource Not Found';
+  err.errors = ['The requested resource could not be found.']
+  err.status = 404;
+  next(err)
+});
+
+app.use((err,  req, res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.titler || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
 
 module.exports = app;
