07a97939fd925fde004efa3671e22c157e47d3b0
jon-wehner <jonjwehner@gmail.com>
2021-01-22 10:56:02 -0800

    login route working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post(
   '/test',
