84966f86d7b1d196ba3bf8e1db51e8f5545f7758
jon-wehner <jonjwehner@gmail.com>
2021-01-22 10:42:37 -0800

    user auth middleware finished

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body })
+router.post(
+  '/test',
+  function(req, res) {
+    res.json({ requestBody: req.body })
 })
 
 module.exports = router;
