ccf31001a7318718bc9802a417dcbf452b2335cd
jon-wehner <jonjwehner@gmail.com>
2021-01-23 04:53:22 -0800

    cleaned up routes, deleted test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,34 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
 
-router.get('/hello/world', function (req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
 
+  router.get(
+    '/',
+    (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend/', 'build', 'index.html')
+      );
+    }
+  );
+  router.get(
+    /^(?!\/?api).*/,
+    (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend,', 'build', 'index.html')
+      );
+    }
+  );
+};
+
+if(process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+};
 
 module.exports = router;
