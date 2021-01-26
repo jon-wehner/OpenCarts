3a55d3e10ac66b4f42578a9756485db11117fd55
jon-wehner <jonjwehner@gmail.com>
2021-01-26 13:12:54 -0800

    seeders adjusteD

--- backend/db/seeders/20210122142408-demo-user.js
+++ backend/db/seeders/20210122142408-demo-user.js
@@ -8,7 +8,7 @@ module.exports = {
     [
       {
         email: 'demo@user.io',
-        username: 'Demo-lition',
+        username: 'demo',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
@@ -23,7 +23,7 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1'] }
+      username: { [Op.in]: ['demo', 'FakeUser1'] }
     }, {});
   }
 };
