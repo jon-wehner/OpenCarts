c8db170142809ab61c2ca2f4a4455e66d0d9259f
jon-wehner <jonjwehner@gmail.com>
2021-01-26 13:58:27 -0800

    fixed seeders

--- backend/db/seeders/20210126190845-seed-states.js
+++ backend/db/seeders/20210126190845-seed-states.js
@@ -7,30 +7,12 @@ module.exports = {
       "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
       "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
     const seedStates = states.map(state => ({ name : state }))
-
-      /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('States',seedStates,{});
   },
 
 
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
     return queryInterface.bulkDelete('States', null, {});
   }
 };
--- backend/db/seeders/20210126212701-carts.js
+++ backend/db/seeders/20210126212701-carts.js
@@ -10,19 +10,18 @@ module.exports = {
         cuisineId: Math.ceil((Math.random()*105)),
         city: faker.address.city(),
         stateId: Math.ceil((Math.random()*51)),
-        zipcode: faker.address.zipCode()
-
+        zipCode: faker.address.zipCode().slice(0,5),
+        imageUrl: faker.image.food()
       }
     }
+    const carts = [];
+    for (let i = 0; i < 10; i++) {
+      carts.push(createCart())
+    }
+    return queryInterface.bulkInsert('Carts', carts, {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Carts', null, {})
   }
 };
