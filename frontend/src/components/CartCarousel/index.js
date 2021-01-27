b3c9f106d6c64a4b020cdcc05c6c4fa1818e8f74
jon-wehner <jonjwehner@gmail.com>
2021-01-27 06:37:27 -0800

    working on rendering only 4 carts at a time

--- frontend/src/components/CartCarousel/index.js
+++ frontend/src/components/CartCarousel/index.js
@@ -1,12 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getCarts } from '../../store/carts'
 import CartDetail from './CartDetail'
+import './CartCarousel.css'
 
 export default function CartCarousel () {
   const dispatch = useDispatch();
   const carts = useSelector(state => Object.values(state.carts))
-
+  /* const [currCarts, setCurrCarts] = useState(carts.slice(0,4));
+  const [prevCarts, setPrevCarts] = useState([]);
+  const [nextCarts, setNextCarts] = useState(carts.slice(3,8));
+  Carts include every cart in our db
+  We want to render 0-3, then 4-7
+  on page load currCarts = carts.slice(0,3) */
 
   useEffect(() => {
     dispatch(getCarts());
@@ -16,11 +22,11 @@ export default function CartCarousel () {
     return null;
   }
   return (
-    <ul>
+    <div className="carousel">
     {carts.map(cart=> {
       return <CartDetail key={cart.id} cart={cart} />
     })}
-    </ul>
+    </div>
   )
 }
 
