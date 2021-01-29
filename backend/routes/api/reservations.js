165d244167016c76ec03e05277df9d57577db0e5
jon-wehner <jonjwehner@gmail.com>
2021-01-29 07:41:13 -0800

    available times coming back from server

--- backend/routes/api/reservations.js
+++ backend/routes/api/reservations.js
@@ -3,7 +3,8 @@ const asyncHandler = require('express-async-handler');
 const router = express.Router();
 const { Cart, Reservation } = require('../../db/models');
 const { Op } = require('sequelize');
-const { date } = require('faker');
+const moment = require('moment')
+
 
 
 
@@ -16,17 +17,14 @@ router.post(
 
     const dateObj = new Date(dateTime)
     const hours = dateObj.getHours()
-    const minTime = new Date(dateTime).setHours(hours+2)
-    console.log(minTime)
+    const minTime = new Date(dateTime).setHours(hours-2)
     const maxTime = new Date(dateTime).setHours(hours+2)
-    console.log(maxTime)
-    const availableTimeslots = {}
+    const timeslots = {}
     let i = minTime
     while (i <= maxTime) {
       i += 900000;
-      availableTimeslots[i] = [];
+      timeslots[i] = [];
     }
-    console.log(availableTimeslots)
     const cartReservations = await Reservation.findAll({
       where: {
         cartId,
@@ -37,19 +35,21 @@ router.post(
           ]
         },
       },
-    })
-    // console.log(cartReservations)
-    // if (cartReservations.length) {
-    //   cartReservations.forEach(reservation => {
-    //     const reservationTime = availableTimeslots[reservation[dateTime]]
-    //     if (reservationTime.length >= 1) {
-    //       delete reservationTime
-    //     } else {
-    //       reservationTime.push(reservation)
-    //     }
-    //   })
-    // }
-    // res.json("hello")
+    });
+    if (cartReservations.length) {
+      cartReservations.forEach(reservation => {
+        const dateUnix = Date.parse(reservation.dateTime)
+        const reservationTime = timeslots[dateUnix]
+        if (reservationTime.length >= 1) {
+          delete reservationTime
+        } else {
+          reservationTime.push(reservation)
+        }
+      })
+    }
+    const keys = Object.keys(timeslots)
+    const availableTimeslots= keys.map(key => moment(parseInt(key, 10)))
+    res.json(availableTimeslots)
   })
 )
 
