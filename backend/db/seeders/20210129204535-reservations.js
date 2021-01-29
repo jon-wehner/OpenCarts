37f1691d663148be7563c9c4b023a0e8933548f7
jon-wehner <jonjwehner@gmail.com>
2021-01-29 12:50:45 -0800

    we reserving some timeslots out here

---
+++ backend/db/seeders/20210129204535-reservations.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Reservations',[
+      { dateTime:"2021-02-07T15:30:00-08:00",
+        partySize:3,
+        cartId: 1,
+        userId: 1
+      }
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Reservations', null, {});
+  }
+};
