dd6631b2c5dd78466bbc0476d1af29b5266aa5ff
jon-wehner <jonjwehner@gmail.com>
2021-04-30 03:11:59 -0700

    put form inputs back into booking area component

--- frontend/src/components/BookingArea/index.js
+++ frontend/src/components/BookingArea/index.js
@@ -1,7 +1,7 @@
-import ReservationSearch from "../ReservationSearch";
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import './BookingArea.css'
+import TimeSelect from './TimeSelect'
 import { getCartsByQuery } from '../../store/carts'
 import { useHistory } from "react-router-dom";
 import { buildReservation } from "../../store/reservations";
@@ -31,10 +31,34 @@ export default function BookingArea () {
       <div className="booking-area__formWrapper">
         <h1 className="booking-area__title">Local & Delicious</h1>
         <form className="booking-area__form" onSubmit={handleSubmit}>
-          <ReservationSearch onDateChange={setDate}
-          onTimeChange={setTime}
-          onPartyChange={setPartySize}
-          onSearchChange={setQuery}/>
+        <div className="reservationSearch__fields">
+          <div className="reservationSearch__inputWrapper">
+            <input className="reservationSearch__inputs" type="date" onChange={e=> setDate(e.target.value)}/>
+            <TimeSelect onTimeChange={setTime}/>
+          </div>
+          <div className="reservationSearch__inputWrapper">
+            <select className="reservationSearch__inputs" onChange={e=> setPartySize(e.target.value)}>
+              <option value="1">1 Person</option>
+              <option value="2">2 People</option>
+              <option value="3">3 People</option>
+              <option value="4">4 People</option>
+              <option value="5">5 People</option>
+              <option value="6">6 People</option>
+              <option value="7">7 People</option>
+              <option value="8">8 People</option>
+              <option value="9">9 People</option>
+              <option value="10">10 People</option>
+            </select>
+          </div>
+          <div>
+            <div className="reservationSearch__inputWrapper">
+              <input className="reservationSearch__inputs"
+              type="text"
+              onChange={e => setQuery(e.target.value)}/>
+            </div>
+            <button className="btn reservationSearch__btn" type="submit">Skip the Line</button>
+          </div>
+        </div>
         </form>
       </div>
 
