77a668a7f20e7fb748737c0fb267303d73286e11
Jon Wehner <jonjwehner@gmail.com>
2021-12-01 16:35:38 -0800

    moved carousel component into booking area

--- frontend/src/components/BookingArea/index.js
+++ frontend/src/components/BookingArea/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate} from 'react-router-dom';
 import TimeSelect from './TimeSelect';
 import tzOffsetToString from '../../utils/utils';
+import CartCarousel from '../CartCarousel';
 import './BookingArea.css';
 
 export default function BookingArea() {
@@ -22,43 +23,46 @@ export default function BookingArea() {
   };
 
   return (
-    <div className="booking-area">
-      <div className="booking-area__formWrapper">
-        <h1 className="booking-area__title">Local & Delicious</h1>
-        <form className="booking-area__form" onSubmit={handleSubmit}>
-          <input
-            className="reservationSearch__inputs"
-            value={date}
-            type="date"
-            onChange={(e) => setDate(e.target.value)}
-          />
-          <TimeSelect onTimeChange={setTime} />
-          <select
-            className="reservationSearch__inputs"
-            onChange={(e) => setPartySize(e.target.value)}
-          >
-            <option value="1">1 Person</option>
-            <option value="2">2 People</option>
-            <option value="3">3 People</option>
-            <option value="4">4 People</option>
-            <option value="5">5 People</option>
-            <option value="6">6 People</option>
-            <option value="7">7 People</option>
-            <option value="8">8 People</option>
-            <option value="9">9 People</option>
-            <option value="10">10 People</option>
-          </select>
-          <input
-            className="reservationSearch__inputs"
-            placeholder="Restaurant or Cuisine"
-            type="text"
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button className="btn reservationSearch__btn" type="submit">
-            Skip the Line
-          </button>
-        </form>
+    <div>
+      <div className="booking-area">
+        <div className="booking-area__formWrapper">
+          <h1 className="booking-area__title">Local & Delicious</h1>
+          <form className="booking-area__form" onSubmit={handleSubmit}>
+            <input
+              className="reservationSearch__inputs"
+              value={date}
+              type="date"
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <TimeSelect onTimeChange={setTime} />
+            <select
+              className="reservationSearch__inputs"
+              onChange={(e) => setPartySize(e.target.value)}
+            >
+              <option value="1">1 Person</option>
+              <option value="2">2 People</option>
+              <option value="3">3 People</option>
+              <option value="4">4 People</option>
+              <option value="5">5 People</option>
+              <option value="6">6 People</option>
+              <option value="7">7 People</option>
+              <option value="8">8 People</option>
+              <option value="9">9 People</option>
+              <option value="10">10 People</option>
+            </select>
+            <input
+              className="reservationSearch__inputs"
+              placeholder="Restaurant or Cuisine"
+              type="text"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button className="btn reservationSearch__btn" type="submit">
+              Skip the Line
+            </button>
+          </form>
+        </div>
       </div>
+        <CartCarousel />
     </div>
   );
 }
