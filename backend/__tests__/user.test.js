530652a7e81256f892bc577a95d43ac9e62b0481
Jon Wehner <jonjwehner@gmail.com>
2022-01-26 00:17:16 -0800

    db

--- backend/__tests__/user.test.js
+++ backend/__tests__/user.test.js
@@ -5,11 +5,12 @@ const supertest = require('supertest');
 
 describe('test the get user reservations route', () => {
   let testDb = db;
-
+  
   it("should respond with 200 and a json payload", async () => {
     const response = await supertest(app)
-      .get('/api/users/1/reservations')
-      .set('Accept', 'application/json')
+    .get('/api/users/1/reservations')
+    .set('Accept', 'application/json')
+    console.log(response)
     expect(response.headers['content-type']).toMatch(/json/);
     expect(response.status).toEqual(200);
   })
