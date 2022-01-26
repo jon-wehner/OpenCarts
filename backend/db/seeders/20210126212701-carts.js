f82c4e049ba6a7cdf3f925d49c33b1244980cc21
Jon Wehner <jonjwehner@gmail.com>
2022-01-26 01:57:22 -0800

    updated cart seeder

--- backend/db/seeders/20210126212701-carts.js
+++ backend/db/seeders/20210126212701-carts.js
@@ -1,18 +1,11 @@
 /* eslint-disable no-unused-vars */
 const faker = require('faker');
-const { createClient } = require('pexels');
+const seedData = require('../seed_data/cartImages.json')
 
-const pexelsApiKey = process.env.PEXELS_API_KEY;
 
-const client = createClient(pexelsApiKey);
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const cartImages = [];
-    const populateImages = async () => {
-      const res = await client.photos.search({ query: 'food truck', per_page: 30 });
-      res.photos.forEach((photo) => cartImages.push(photo.src.medium));
-    };
-    await populateImages();
+    const cartImages = seedData.urls;    
     const createCart = () => ({
       name: faker.lorem.slug(),
       address: faker.address.streetAddress(),
@@ -21,7 +14,7 @@ module.exports = {
       city: faker.address.city(),
       stateId: Math.ceil(Math.random() * 51),
       zipCode: faker.address.zipCode().slice(0, 5),
-      imageUrl: cartImages[Math.floor(Math.random() * 30)],
+      imageUrl: cartImages[Math.floor(Math.random() * 19)],
     });
     const carts = [];
     for (let i = 0; i < 20; i += 1) {
