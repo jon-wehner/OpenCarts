33373eb226a9870564e976c83c57c12dc91cb7ec
Jon Wehner <jonjwehner@gmail.com>
2022-02-22 23:29:56 -0800

    typescript migration, eslint config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [2, {
+      'extensions': [
+          '.jsx',
+          '.tsx'
+      ]
+    }],
+    'react/require-default-props': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0
+  }
+};
