057a502ef1a76e5e3e5cc43e096a04b5f62eeefe
Jon Wehner <jonjwehner@gmail.com>
2022-03-02 18:45:16 -0800

    setup error handling in store

--- frontend/src/store/session.ts
+++ frontend/src/store/session.ts
@@ -1,18 +1,24 @@
 import { AnyAction } from 'redux';
 import { AppDispatch } from '.';
-import { CustomResponse, User } from '../interfaces';
+import { CustomResponse, Error, User } from '../interfaces';
 import { fetch } from './csrf';
 
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
+const SET_ERRORS = 'session/setErrors';
 
 const setUser = (user: User) => ({
   type: SET_USER,
-  user,
+  payload: user,
 });
 
 const removeUser = () => ({
   type: REMOVE_USER,
+});
+
+const setErrors = (errors: Error) => ({
+  type: SET_ERRORS,
+  payload: errors,
 });
 
 export const loginUser = (user: {credential: string, password: string}) => async (dispatch: AppDispatch) => {
@@ -20,8 +26,12 @@ export const loginUser = (user: {credential: string, password: string}) => async
     method: 'POST',
     body: JSON.stringify(user),
   };
-  const res: CustomResponse = await fetch('/api/session', options);
-  dispatch(setUser(res.data.user));
+  try {
+    const res: CustomResponse = await fetch('/api/session', options);
+    dispatch(setUser(res.data.user));
+  } catch (err: any) {
+    dispatch(setErrors(err.data.errors));
+  }
 };
 
 export const restoreUser = () => async (dispatch: AppDispatch) => {
@@ -65,7 +75,7 @@ export default function sessionReducer(state = initialState, action: AnyAction) 
     case SET_USER: {
       const newState = {
         ...state,
-        user: action.user,
+        user: action.payload,
       };
       return newState;
     }
@@ -73,6 +83,13 @@ export default function sessionReducer(state = initialState, action: AnyAction) 
       const newState = {
         ...state,
         user: null,
+      };
+      return newState;
+    }
+    case SET_ERRORS: {
+      const newState = {
+        ...state,
+        errors: action.payload,
       };
       return newState;
     }
