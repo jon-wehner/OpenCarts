6b7174bf082a9bfa1409418022ea93b11f4ba620
Jon Wehner <jonjwehner@gmail.com>
2022-08-11 22:51:18 -0700

    working on cart profile page

--- frontend/src/components/CartProfile/index.tsx
+++ frontend/src/components/CartProfile/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function CartProfile() {
+  // TODO: this component really needs to utilize useParams but it can come back undefined. Figure it out.
   return (
     <p>
       here lyeth the profile of the cart you clicked cart id:
