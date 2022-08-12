fbae35f83d58b0bb44073e146106502fd921c5ad
Jon Wehner <jonjwehner@gmail.com>
2022-08-11 21:43:44 -0700

    setup cart profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Navigation from './components/Navigation';
 import BookingArea from './components/BookingArea';
 import SearchResults from './components/SearchResults';
 import UserProfile from './components/UserProfile';
+import CartProfile from './components/CartProfile';
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" element={<BookingArea />} />
           <Route path="/search" element={<SearchResults />} />
           <Route path="/profile" element={<UserProfile />} />
+          <Route path="/cart/:id" element={<CartProfile />} />
         </Routes>
       </div>
     </>
---
+++ frontend/src/components/CartProfile/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CartProfile() {
+  return (
+    <p>here lyeth the profile of the cart you clicked</p>
+  );
+}
